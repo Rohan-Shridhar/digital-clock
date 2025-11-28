653607d20a23d1f0068657ef9a4215e5968e762b
Rohan-Shridhar <rohansm668@gmail.com>
2025-11-28 22:11:43 +0530

    Updated jpg to gif

--- src/BackgroundChanger.jsx
+++ src/BackgroundChanger.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import img1 from "/public/totoro_1.jpeg";
 import img2 from "/public/totoro_2.jpeg";
 import img3 from "/public/totoro_3.jpeg";
-import btn from "/public/btn.png";
+import btn from "/public/btn.gif";
 
 export default function BackgroundChanger() {
     const images = [img1, img2, img3];
